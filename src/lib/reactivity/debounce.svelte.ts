bc165cfe117340450a903ef3b2f3a0043c216dc2
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-12-16 16:47:03 +0100

    feat: added debouncedState in reactivity module

---
+++ src/lib/reactivity/debounce.svelte.ts
@@ -0,0 +1,27 @@
+type DebouncedStateOptions = {
+	debounce?: number;
+};
+
+const DEFAULT_DEBOUNCE = 1000;
+
+export function debouncedState<T>(initial: T, options?: DebouncedStateOptions) {
+	const debounce = options?.debounce ?? DEFAULT_DEBOUNCE;
+
+	let timeout: NodeJS.Timeout;
+	let _current = $state<T>(initial);
+
+	return {
+		get current() {
+			return _current;
+		},
+		set current(v: T) {
+			if (timeout) {
+				clearTimeout(timeout);
+			}
+
+			timeout = setTimeout(() => {
+				_current = v;
+			}, debounce);
+		}
+	};
+}
