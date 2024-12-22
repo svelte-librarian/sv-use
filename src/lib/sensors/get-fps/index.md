e5492aff637c46a2acb9ee1e3214baf670d0ae56
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-12-22 13:16:26 +0100

    feat: added getFps utility

---
+++ src/lib/sensors/get-fps/index.md
@@ -0,0 +1,16 @@
+---
+slug: "get-fps"
+title: "getFps"
+description: "Get the current frames per second of the device."
+category: "sensors"
+---
+
+## Usage
+
+```svelte
+<script lang="ts">
+    import { getFps } from "@svelte-use/core";
+
+    const fps = getFps();
+</script>
+```
---
+++ src/lib/sensors/get-fps/index.svelte.ts
@@ -0,0 +1,45 @@
+import { onMount } from 'svelte';
+
+type GetFpsOptions = {
+	/** Re-calculate the frames per second every `x` frames. */
+	every?: number;
+};
+
+type GetFpsReturn = {
+	readonly current: number;
+};
+
+export function getFps(options: GetFpsOptions = {}): GetFpsReturn {
+	const { every = 10 } = options;
+
+	let _fps = $state<number>(0);
+
+	let last = performance.now();
+	let ticks = 0;
+
+	onMount(() => {
+		const callback = () => {
+			ticks += 1;
+
+			if (ticks >= every) {
+				const now = performance.now();
+				const delta = now - last;
+
+				_fps = Math.round(1000 / (delta / ticks));
+
+				last = now;
+				ticks = 0;
+			}
+
+			window.requestAnimationFrame(callback);
+		};
+
+		window.requestAnimationFrame(callback);
+	});
+
+	return {
+		get current() {
+			return _fps;
+		}
+	};
+}
