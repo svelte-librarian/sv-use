dbfc6d45dbb05107501a9e5eb0365ce934839dc9
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-12-27 02:56:20 +0100

    docs: added docs for createFileDialog browser utility

---
+++ packages/website/src/lib/docs/core/browser/create-file-dialog/Demo.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { createFileDialog } from '$sv-use/core';
+
+	const dialog = createFileDialog({
+		accept: 'image/*',
+		multiple: true,
+		onChange(files) {
+			console.log($state.snapshot(files));
+		},
+		onCancel() {
+			console.log('cancelled');
+		}
+	});
+</script>
+
+<div class="relative flex w-full flex-col gap-2">
+	<button onclick={dialog.open} class="bg-svelte rounded-md px-3 py-1 text-white">
+		Open file dialog
+	</button>
+	<button
+		onclick={dialog.reset}
+		disabled={dialog.files.length === 0}
+		class="bg-svelte rounded-md px-3 py-1 text-white disabled:cursor-not-allowed disabled:opacity-50"
+	>
+		Reset
+	</button>
+	<div class="flex flex-col gap-5">
+		Selected Files ({dialog.files.length})
+		<ul>
+			{#each dialog.files as file (file.name)}
+				<li>{file.name}</li>
+			{:else}
+				<p class="italic">Empty...</p>
+			{/each}
+		</ul>
+	</div>
+</div>
