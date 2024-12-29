feed3672f2359d53906bef2c7bac64a2e00614e1
Sajidur Rahman <rahmansajidur30@gmail.com>
2024-12-29 19:22:22 +0100

    test(core): historyState -> added failing tests in comment for when it is fixed

--- packages/core/src/states/history-state/index.svelte.test.ts
+++ packages/core/src/states/history-state/index.svelte.test.ts
@@ -154,3 +154,83 @@ describe('Works with reassigned objects', () => {
 		expect(history.current).toStrictEqual({ value: 'test2' });
 	});
 });
+
+// TODO : Make historyState track assignments on nested objects
+
+/* describe('Works with property assignment on objects', () => {
+	it('Adds to the history correctly with one change', () => {
+		const history = historyState({ value: '' });
+
+		history.current.value = 'test';
+
+		expect(history.history.length).toBe(1);
+		expect(history.history[0].snapshot).toStrictEqual({ value: '' });
+		expect(history.history[0].timestamp).toBeGreaterThan(0);
+
+		expect(history.current).toStrictEqual({ value: 'test' });
+	});
+
+	it('Adds to the history correctly with multiple changes', () => {
+		const history = historyState({ value: '' });
+
+		history.current.value = 'test';
+		history.current.value = 'test2';
+
+		expect(history.history.length).toBe(2);
+		expect(history.history[0].snapshot).toStrictEqual({ value: '' });
+		expect(history.history[0].timestamp).toBeGreaterThan(0);
+		expect(history.history[1].snapshot).toStrictEqual({ value: 'test' });
+		expect(history.history[1].timestamp).toBeGreaterThan(0);
+
+		expect(history.current).toStrictEqual({ value: 'test2' });
+	});
+
+	it('Undoes correctly', () => {
+		const history = historyState({ value: '' });
+
+		history.current.value = 'test';
+		history.current.value = 'test2';
+
+		expect(history.history.length).toBe(2);
+		expect(history.history[0].snapshot).toStrictEqual({ value: '' });
+		expect(history.history[0].timestamp).toBeGreaterThan(0);
+		expect(history.history[1].snapshot).toStrictEqual({ value: 'test' });
+		expect(history.history[1].timestamp).toBeGreaterThan(0);
+
+		expect(history.current).toStrictEqual({ value: 'test2' });
+
+		history.undo();
+
+		expect(history.history.length).toBe(1);
+		expect(history.history[0].snapshot).toStrictEqual({ value: '' });
+		expect(history.history[0].timestamp).toBeGreaterThan(0);
+
+		expect(history.current).toStrictEqual({ value: 'test' });
+	});
+
+	it('Redoes correctly', () => {
+		const history = historyState({ value: '' });
+
+		history.current.value = 'test';
+		history.current.value = 'test2';
+
+		expect(history.history.length).toBe(2);
+		expect(history.history[0].snapshot).toStrictEqual({ value: '' });
+		expect(history.history[0].timestamp).toBeGreaterThan(0);
+		expect(history.history[1].snapshot).toStrictEqual({ value: 'test' });
+		expect(history.history[1].timestamp).toBeGreaterThan(0);
+
+		expect(history.current).toStrictEqual({ value: 'test2' });
+
+		history.undo();
+		history.redo();
+
+		expect(history.history.length).toBe(2);
+		expect(history.history[0].snapshot).toStrictEqual({ value: '' });
+		expect(history.history[0].timestamp).toBeGreaterThan(0);
+		expect(history.history[1].snapshot).toStrictEqual({ value: 'test' });
+		expect(history.history[1].timestamp).toBeGreaterThan(0);
+
+		expect(history.current).toStrictEqual({ value: 'test2' });
+	});
+}); */
