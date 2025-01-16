4c1fa0a78f11b7f86b91d5af0f6477c0b0b5f161
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-01-16 20:02:06 +0100

    feat(core): improved docs and comments

--- packages/core/src/observers/observe-mutation/index.svelte.ts
+++ packages/core/src/observers/observe-mutation/index.svelte.ts
@@ -2,21 +2,27 @@ import { watch } from '../../reactivity/watch/index.svelte.js';
 import { isSupported } from '../../__internal__/is.svelte.js';
 import { defaultWindow, type ConfigurableWindow } from '../../__internal__/configurable.js';
 import { normalizeValue, notNullish, toArray } from '../../__internal__/utils.js';
-import type { Arrayable, MaybeGetter } from '../../__internal__/types.js';
+import type { Arrayable, CleanupFunction, MaybeGetter } from '../../__internal__/types.js';
 
 interface ObserveMutationOptions extends MutationObserverInit, ConfigurableWindow {}
 
 type ObserveMutationReturn = {
+	/**
+	 * Whether the {@link https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver | `Mutation Observer API`} is supported or not.
+	 * @see https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver#browser_compatibility
+	 */
 	readonly isSupported: boolean;
-	cleanup: () => void;
+	/** A function to cleanup the observer. */
+	cleanup: CleanupFunction;
+	/** Empties the record queue and returns what was in there. */
 	takeRecords: () => void;
 };
 
 /**
  * Watch for changes being made to the DOM tree.
- * @param targets
- * @param callback
- * @param options
+ * @param targets The targets to observe.
+ * @param callback The callback for when a change is detected.
+ * @param options Additional options to customize the behavior.
  */
 export function observeMutation(
 	targets: Arrayable<MaybeGetter<HTMLElement | undefined>>,
@@ -25,9 +31,9 @@ export function observeMutation(
 ): ObserveMutationReturn {
 	const { window = defaultWindow, ...mutationOptions } = options;
 
-	let observer: MutationObserver | undefined;
-	const _isSupported = isSupported(() => window !== undefined && 'MutationObserver' in window);
+	let _observer: MutationObserver | undefined;
 
+	const _isSupported = isSupported(() => window !== undefined && 'MutationObserver' in window);
 	const _targets = $derived(
 		new Set(toArray(targets).map<HTMLElement | undefined>(normalizeValue).filter(notNullish))
 	);
@@ -38,22 +44,22 @@ export function observeMutation(
 			cleanup();
 
 			if (_isSupported.current && targets.size) {
-				observer = new MutationObserver(callback);
-				targets.forEach((el) => observer!.observe(el, mutationOptions));
+				_observer = new MutationObserver(callback);
+				targets.forEach((el) => _observer!.observe(el, mutationOptions));
 			}
 		},
 		{ runOnMounted: true }
 	);
 
 	function takeRecords() {
-		return observer?.takeRecords();
+		return _observer?.takeRecords();
 	}
 
 	function cleanup() {
-		if (observer) {
-			observer.disconnect();
-			observer = undefined;
-		}
+		if (!_observer) return;
+
+		_observer.disconnect();
+		_observer = undefined;
 	}
 
 	return {
