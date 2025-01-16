ac01295e51f06105775f20e4962fd10c555416b0
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-01-16 22:18:48 +0100

    feat(core): added observeResize utility

--- packages/core/src/observers/index.ts
+++ packages/core/src/observers/index.ts
@@ -1,2 +1,3 @@
 export * from './observe-intersection/index.svelte.js';
 export * from './observe-mutation/index.svelte.js';
+export * from './observe-resize/index.svelte.js';
---
+++ packages/website/src/lib/docs/core/observers/observe-resize/Demo.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { observeResize } from '$sv-use/core';
+
+	let el = $state<HTMLTextAreaElement>();
+	let text = $state('');
+
+	observeResize(
+		() => el,
+		(entries) => {
+			const { width, height } = entries[0].contentRect;
+
+			text = `width: ${width.toFixed(0)}px\nheight: ${height.toFixed(0)}px`;
+		}
+	);
+</script>
+
+<div class="relative flex w-full flex-col items-start gap-2">
+	<span class="text-sm italic text-zinc-500">Resize the box to see changes</span>
+	<textarea bind:this={el} bind:value={text} class="resize rounded-md p-5" disabled></textarea>
+</div>
