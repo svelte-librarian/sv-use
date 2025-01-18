efbd3e1dc1fef2bffa295a96f02c11c84ad30b36
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-01-18 19:08:57 +0100

    fix(core): exposing defaultState in package's entrypoint

--- packages/core/src/states/index.ts
+++ packages/core/src/states/index.ts
@@ -1,6 +1,7 @@
 export * from './async-state/index.svelte.js';
 export * from './auto-reset-state/index.svelte.js';
 export * from './debounced-state/index.svelte.js';
+export * from './default-state/index.svelte.js';
 export * from './history-state/index.svelte.js';
 export * from './local-state/index.svelte.js';
 export * from './session-state/index.svelte.js';
