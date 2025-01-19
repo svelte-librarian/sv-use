8d0f530eee660876f56c2e3153c249bef2dc36e8
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-01-19 12:48:57 +0100

    feat(core): added options.autoCleanup for getDeviceOrientation

--- packages/core/src/sensors/get-device-orientation/index.svelte.ts
+++ packages/core/src/sensors/get-device-orientation/index.svelte.ts
@@ -1,6 +1,18 @@
-import { onMount } from 'svelte';
-import { isSupported } from '../../__internal__/is.svelte.js';
+import { onDestroy } from 'svelte';
 import { handleEventListener } from '../../browser/index.js';
+import { isSupported } from '../../__internal__/is.svelte.js';
+import { noop } from '../../__internal__/utils.js';
+import type { CleanupFunction } from '../../__internal__/types.js';
+
+type GetDeviceOrientationOptions = {
+	/**
+	 * Whether to auto-cleanup the event listener or not.
+	 *
+	 * If set to `true`, it must run in the component initialization lifecycle.
+	 * @default true
+	 */
+	autoCleanup?: boolean;
+};
 
 type GetDeviceOrientationReturn = {
 	readonly isSupported: boolean;
@@ -12,31 +24,44 @@ type GetDeviceOrientationReturn = {
 	readonly beta: number;
 	/** The motion of the device around the y axis, express in degrees with values ranging from -90 (inclusive) to 90 (exclusive). */
 	readonly gamma: number;
+	/**
+	 * Cleans up the event listener.
+	 * @note Is called automatically if `options.autoCleanup` is set to `true`.
+	 */
+	cleanup: CleanupFunction;
 };
 
 /**
  * Provides web developers with information from the physical orientation of the device running the web page.
  * @see https://svelte-librarian.github.io/sv-use/docs/core/sensors/get-device-orientation
  */
-export function getDeviceOrientation(): GetDeviceOrientationReturn {
+export function getDeviceOrientation(
+	options: GetDeviceOrientationOptions = {}
+): GetDeviceOrientationReturn {
+	const { autoCleanup = true } = options;
+
+	let cleanup: CleanupFunction = noop;
+
 	const _isSupported = isSupported(() => window && 'DeviceOrientationEvent' in window);
 	let _isAbsolute = $state<boolean>(false);
 	let _alpha = $state<number>(0);
 	let _beta = $state<number>(0);
 	let _gamma = $state<number>(0);
 
-	onMount(() => {
-		if (!_isSupported.current) return;
+	if (_isSupported.current) {
+		cleanup = handleEventListener('deviceorientation', onDeviceOrientation, { autoCleanup });
+	}
 
-		const onDeviceOrientation = (event: DeviceOrientationEvent) => {
-			_isAbsolute = event.absolute;
-			_alpha = event.alpha!;
-			_beta = event.beta!;
-			_gamma = event.gamma!;
-		};
+	if (autoCleanup) {
+		onDestroy(() => cleanup());
+	}
 
-		return handleEventListener('deviceorientation', onDeviceOrientation);
-	});
+	function onDeviceOrientation(event: DeviceOrientationEvent) {
+		_isAbsolute = event.absolute;
+		_alpha = event.alpha!;
+		_beta = event.beta!;
+		_gamma = event.gamma!;
+	}
 
 	return {
 		get isSupported() {
@@ -53,6 +78,7 @@ export function getDeviceOrientation(): GetDeviceOrientationReturn {
 		},
 		get gamma() {
 			return _gamma;
-		}
+		},
+		cleanup
 	};
 }
