bc6d403f9db7fd1114e878873820938d4aab0b3c
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-01-19 13:05:26 +0100

    feat(core): added options.autoCleanup for getMouse

--- packages/core/src/sensors/get-mouse/index.svelte.ts
+++ packages/core/src/sensors/get-mouse/index.svelte.ts
@@ -1,9 +1,26 @@
-import { onMount } from 'svelte';
+import { onDestroy } from 'svelte';
+import { BROWSER } from 'esm-env';
+import { handleEventListener } from '../../browser/index.js';
+import { noop } from '../../__internal__/utils.js';
+import type { CleanupFunction } from '../../__internal__/types.js';
 
 type GetMouseOptions = {
-	/** The initial value for the position of the mouse. */
-	initial: { x: number; y: number };
-	/** A callback for when the mouse moves. */
+	/**
+	 * Whether to auto-cleanup the event listener or not.
+	 *
+	 * If set to `true`, it must run in the component initialization lifecycle.
+	 * @default true
+	 */
+	autoCleanup?: boolean;
+	/**
+	 * The initial position of the mouse.
+	 * @default { x: 0; y: 0 }
+	 */
+	initial?: { x: number; y: number };
+	/**
+	 * A callback for when the mouse moves.
+	 * @default () => {}
+	 */
 	onMove?: (event: MouseEvent) => void;
 };
 
@@ -12,34 +29,40 @@ type GetMouseReturn = {
 	readonly x: number;
 	/** The vertical position of the mouse. */
 	readonly y: number;
+	/**
+	 * Cleans up the event listener.
+	 * @note Is called automatically if `options.autoCleanup` is set to `true`.
+	 */
+	cleanup: CleanupFunction;
 };
-
-const DEFAULT_INITIAL = { x: 0, y: 0 };
 
 /**
  * Retrieves information about the mouse.
  * @param options Additional options to customize the behavior.
  * @see https://svelte-librarian.github.io/sv-use/docs/core/sensors/get-mouse
  */
-export function getMouse(options?: GetMouseOptions): GetMouseReturn {
-	const DEFAULT_ON_MOVE = (event: MouseEvent) => {
-		_x = event.pageX;
-		_y = event.pageY;
-	};
+export function getMouse(options: GetMouseOptions = {}): GetMouseReturn {
+	const { autoCleanup = true, initial = { x: 0, y: 0 }, onMove = noop } = options;
 
-	const initial = options?.initial ?? DEFAULT_INITIAL;
-	const onMove = options?.onMove ?? DEFAULT_ON_MOVE;
+	let cleanup: CleanupFunction = noop;
 
 	let _x = $state<number>(initial.x);
 	let _y = $state<number>(initial.y);
 
-	onMount(() => {
-		window.addEventListener('mousemove', onMove);
+	if (BROWSER) {
+		cleanup = handleEventListener('mousemove', onMouseMove, { autoCleanup });
+	}
 
-		return () => {
-			window.removeEventListener('mousemove', onMove);
-		};
-	});
+	if (autoCleanup) {
+		onDestroy(() => cleanup());
+	}
+
+	function onMouseMove(event: MouseEvent) {
+		_x = event.pageX;
+		_y = event.pageY;
+
+		onMove(event);
+	}
 
 	return {
 		get x() {
@@ -47,6 +70,7 @@ export function getMouse(options?: GetMouseOptions): GetMouseReturn {
 		},
 		get y() {
 			return _y;
-		}
+		},
+		cleanup
 	};
 }
