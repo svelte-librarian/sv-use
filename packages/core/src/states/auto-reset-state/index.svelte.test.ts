1314660d48d89befd12967380ddf789f2250342d
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-01-19 17:39:11 +0100

    chore(core): reworked autoResetState to use $effect

--- packages/core/src/states/auto-reset-state/index.svelte.test.ts
+++ packages/core/src/states/auto-reset-state/index.svelte.test.ts
@@ -1,5 +1,6 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { autoResetState } from './index.svelte.js';
+import { flushSync } from 'svelte';
 
 const DEFAULT_DELAY = 3000;
 const DEFAULT_CHECK_AFTER = DEFAULT_DELAY + 100;
@@ -16,59 +17,86 @@ describe('Works with primitives', () => {
 	});
 
 	it('Resets properly after the default delay', () => {
-		const message = autoResetState('');
+		const cleanup = $effect.root(() => {
+			const message = autoResetState('');
 
-		message.current = 'test';
-		expect(message.current).toBe('test');
+			flushSync(() => {
+				message.current = 'test';
+			});
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER / 2);
-		expect(message.current).toBe('test');
+			expect(message.current).toBe('test');
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
-		expect(message.current).toBe('');
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER / 2);
+			expect(message.current).toBe('test');
+
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
+			expect(message.current).toBe('');
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after a custom delay', () => {
-		const message = autoResetState('', CUSTOM_DELAY);
+		const cleanup = $effect.root(() => {
+			const message = autoResetState('', CUSTOM_DELAY);
 
-		message.current = 'test';
-		expect(message.current).toBe('test');
+			flushSync(() => {
+				message.current = 'test';
+			});
+			expect(message.current).toBe('test');
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toBe('test');
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toBe('test');
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
-		expect(message.current).toBe('');
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
+			expect(message.current).toBe('');
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after the default delay after multiple changes', () => {
-		const message = autoResetState('');
+		const cleanup = $effect.root(() => {
+			const message = autoResetState('');
 
-		message.current = 'test';
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toBe('test');
+			flushSync(() => {
+				message.current = 'test';
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toBe('test');
 
-		message.current = 'test2';
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toBe('test2');
+			message.current = 'test2';
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toBe('test2');
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
-		expect(message.current).toBe('');
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
+			expect(message.current).toBe('');
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after a custom delay after multiple changes', () => {
-		const message = autoResetState('', CUSTOM_DELAY);
+		const cleanup = $effect.root(() => {
+			const message = autoResetState('', CUSTOM_DELAY);
 
-		message.current = 'test';
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toBe('test');
+			flushSync(() => {
+				message.current = 'test';
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toBe('test');
 
-		message.current = 'test2';
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toBe('test2');
+			flushSync(() => {
+				message.current = 'test2';
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toBe('test2');
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
-		expect(message.current).toBe('');
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
+			expect(message.current).toBe('');
+		});
+
+		cleanup();
 	});
 });
 
@@ -82,59 +110,87 @@ describe('Works with objects', () => {
 	});
 
 	it('Resets properly after the default delay', () => {
-		const message = autoResetState({ value: '' });
+		const cleanup = $effect.root(() => {
+			const message = autoResetState({ value: '' });
 
-		message.current = { value: 'test' };
-		expect(message.current).toStrictEqual({ value: 'test' });
+			flushSync(() => {
+				message.current = { value: 'test' };
+			});
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test' });
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
-		expect(message.current).toStrictEqual({ value: '' });
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
+			expect(message.current).toStrictEqual({ value: '' });
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after a custom delay', () => {
-		const message = autoResetState({ value: '' }, CUSTOM_DELAY);
+		const cleanup = $effect.root(() => {
+			const message = autoResetState({ value: '' }, CUSTOM_DELAY);
 
-		message.current = { value: 'test' };
-		expect(message.current).toStrictEqual({ value: 'test' });
+			flushSync(() => {
+				message.current = { value: 'test' };
+			});
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test' });
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
-		expect(message.current).toStrictEqual({ value: '' });
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
+			expect(message.current).toStrictEqual({ value: '' });
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after the default delay after multiple changes', () => {
-		const message = autoResetState({ value: '' });
+		const cleanup = $effect.root(() => {
+			const message = autoResetState({ value: '' });
 
-		message.current = { value: 'test' };
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test' });
+			flushSync(() => {
+				message.current = { value: 'test' };
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		message.current = { value: 'test2' };
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test2' });
+			flushSync(() => {
+				message.current = { value: 'test2' };
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test2' });
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
-		expect(message.current).toStrictEqual({ value: '' });
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
+			expect(message.current).toStrictEqual({ value: '' });
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after a custom delay after multiple changes', () => {
-		const message = autoResetState({ value: '' }, CUSTOM_DELAY);
+		const cleanup = $effect.root(() => {
+			const message = autoResetState({ value: '' }, CUSTOM_DELAY);
 
-		message.current = { value: 'test' };
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test' });
+			flushSync(() => {
+				message.current = { value: 'test' };
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		message.current = { value: 'test2' };
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test2' });
+			flushSync(() => {
+				message.current = { value: 'test2' };
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test2' });
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
-		expect(message.current).toStrictEqual({ value: '' });
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
+			expect(message.current).toStrictEqual({ value: '' });
+		});
+
+		cleanup();
 	});
 });
 
@@ -148,58 +204,86 @@ describe('Works with property assignment on objects', () => {
 	});
 
 	it('Resets properly after the default delay', () => {
-		const message = autoResetState({ value: '' });
+		const cleanup = $effect.root(() => {
+			const message = autoResetState({ value: '' });
 
-		message.current.value = 'test';
-		expect(message.current).toStrictEqual({ value: 'test' });
+			flushSync(() => {
+				message.current.value = 'test';
+			});
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test' });
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
-		expect(message.current).toStrictEqual({ value: '' });
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
+			expect(message.current).toStrictEqual({ value: '' });
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after a custom delay', () => {
-		const message = autoResetState({ value: '' }, CUSTOM_DELAY);
+		const cleanup = $effect.root(() => {
+			const message = autoResetState({ value: '' }, CUSTOM_DELAY);
 
-		message.current.value = 'test';
-		expect(message.current).toStrictEqual({ value: 'test' });
+			flushSync(() => {
+				message.current.value = 'test';
+			});
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test' });
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
-		expect(message.current).toStrictEqual({ value: '' });
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
+			expect(message.current).toStrictEqual({ value: '' });
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after the default delay after multiple changes', () => {
-		const message = autoResetState({ value: '' });
+		const cleanup = $effect.root(() => {
+			const message = autoResetState({ value: '' });
 
-		message.current.value = 'test';
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test' });
+			flushSync(() => {
+				message.current.value = 'test';
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		message.current.value = 'test2';
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test2' });
+			flushSync(() => {
+				message.current.value = 'test2';
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test2' });
 
-		vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
-		expect(message.current).toStrictEqual({ value: '' });
+			vi.advanceTimersByTime(DEFAULT_CHECK_AFTER);
+			expect(message.current).toStrictEqual({ value: '' });
+		});
+
+		cleanup();
 	});
 
 	it('Resets properly after a custom delay after multiple changes', () => {
-		const message = autoResetState({ value: '' }, CUSTOM_DELAY);
+		const cleanup = $effect.root(() => {
+			const message = autoResetState({ value: '' }, CUSTOM_DELAY);
 
-		message.current.value = 'test';
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test' });
+			flushSync(() => {
+				message.current.value = 'test';
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test' });
 
-		message.current.value = 'test2';
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
-		expect(message.current).toStrictEqual({ value: 'test2' });
+			flushSync(() => {
+				message.current.value = 'test2';
+			});
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER / 2);
+			expect(message.current).toStrictEqual({ value: 'test2' });
 
-		vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
-		expect(message.current).toStrictEqual({ value: '' });
+			vi.advanceTimersByTime(CUSTOM_CHECK_AFTER);
+			expect(message.current).toStrictEqual({ value: '' });
+		});
+
+		cleanup();
 	});
 });
