87ad05ba1795e4097e749754af7b59c71ae93fb3
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-01-19 12:40:30 +0100

    feat(core): added options.autoCleanup for getDeviceMotion

--- packages/core/src/sensors/get-device-motion/index.svelte.ts
+++ packages/core/src/sensors/get-device-motion/index.svelte.ts
@@ -1,6 +1,18 @@
-import { onMount } from 'svelte';
+import { onDestroy } from 'svelte';
 import { handleEventListener } from '../../browser/index.js';
 import { isSupported } from '../../__internal__/is.svelte.js';
+import { noop } from '../../__internal__/utils.js';
+import type { CleanupFunction } from '../../__internal__/types.js';
+
+type GetDeviceMotionOptions = {
+	/**
+	 * Whether to auto-cleanup the event listener or not.
+	 *
+	 * If set to `true`, it must run in the component initialization lifecycle.
+	 * @default true
+	 */
+	autoCleanup?: boolean;
+};
 
 type GetDeviceMotionReturn = {
 	/** Whether the device supports the {@link https://developer.mozilla.org/en-US/docs/Web/API/DeviceMotionEvent | `DeviceMotionEvent`} feature or not. */
@@ -23,13 +35,22 @@ type GetDeviceMotionReturn = {
 	readonly rotationRate: DeviceMotionEventRotationRate;
 	/** The interval, in milliseconds, at which data is obtained from the underlying hardware. */
 	readonly interval: number;
+	/**
+	 * Cleans up the event listener.
+	 * @note Is called automatically if `options.autoCleanup` is set to `true`.
+	 */
+	cleanup: CleanupFunction;
 };
 
 /**
  * Provides information about the device's motion, including acceleration and rotation rate.
  * @see https://svelte-librarian.github.io/sv-use/docs/core/sensors/get-device-motion
  */
-export function getDeviceMotion(): GetDeviceMotionReturn {
+export function getDeviceMotion(options: GetDeviceMotionOptions = {}): GetDeviceMotionReturn {
+	const { autoCleanup = true } = options;
+
+	let cleanup: CleanupFunction = noop;
+
 	const _isSupported = isSupported(() => window !== undefined && 'DeviceMotionEvent' in window);
 	let _acceleration = $state<NonNullable<DeviceMotionEvent['acceleration']>>({
 		x: null,
@@ -50,34 +71,36 @@ export function getDeviceMotion(): GetDeviceMotionReturn {
 	});
 	let _interval = $state<number>(0);
 
-	onMount(() => {
-		if (!_isSupported.current) return;
+	if (_isSupported.current) {
+		cleanup = handleEventListener('devicemotion', onDeviceMotion, { autoCleanup });
+	}
 
-		const onDeviceMotion = (event: DeviceMotionEvent) => {
-			if (event.acceleration) {
-				_acceleration = event.acceleration;
-			}
+	if (autoCleanup) {
+		onDestroy(() => cleanup());
+	}
 
-			if (event.accelerationIncludingGravity) {
-				_accelerationIncludingGravity = event.accelerationIncludingGravity;
-			}
+	function onDeviceMotion(event: DeviceMotionEvent) {
+		if (event.acceleration) {
+			_acceleration = event.acceleration;
+		}
 
-			if (event.rotationRate) {
-				_rotationRate = event.rotationRate;
-			}
+		if (event.accelerationIncludingGravity) {
+			_accelerationIncludingGravity = event.accelerationIncludingGravity;
+		}
 
-			_interval = event.interval;
-		};
+		if (event.rotationRate) {
+			_rotationRate = event.rotationRate;
+		}
 
-		return handleEventListener('devicemotion', onDeviceMotion);
-	});
+		_interval = event.interval;
+	}
 
 	return {
 		get isSupported() {
 			return _isSupported.current;
 		},
 		get acceleration() {
-			return _acceleration ?? { x: null, y: null, z: null };
+			return _acceleration;
 		},
 		get accelerationIncludingGravity() {
 			return _accelerationIncludingGravity;
@@ -87,6 +110,7 @@ export function getDeviceMotion(): GetDeviceMotionReturn {
 		},
 		get interval() {
 			return _interval;
-		}
+		},
+		cleanup
 	};
 }
