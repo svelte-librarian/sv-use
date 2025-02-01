0b92193f2755df6de4035baa3ef49056a703d6e3
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-02-01 22:01:55 +0100

    feat(core): added getElementSize utility

--- packages/core/src/elements/index.ts
+++ packages/core/src/elements/index.ts
@@ -1,4 +1,5 @@
 export * from './create-drop-zone/index.svelte.js';
 export * from './get-active-element/index.svelte.js';
 export * from './get-document-visibility/index.svelte.js';
+export * from './get-element-size/index.svelte.js';
 export * from './is-window-focused/index.svelte.js';
