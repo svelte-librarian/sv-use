dbe152f15aaafbb048f8b2c55327f79bd5106159
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-02-01 15:23:36 +0100

    feat(core): added handleWakeLock utility

--- packages/core/src/browser/index.ts
+++ packages/core/src/browser/index.ts
@@ -4,3 +4,4 @@ export * from './create-web-notification/index.svelte.js';
 export * from './get-clipboard-text/index.svelte.js';
 export * from './get-permission/index.svelte.js';
 export * from './handle-event-listener/index.svelte.js';
+export * from './handle-wake-lock/index.svelte.js';
