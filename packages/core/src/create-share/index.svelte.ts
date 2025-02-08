6c1306ad15aca425b98b235b36cea84a24e87650
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-02-09 00:18:05 +0100

    feat(core): added createShare utility

---
+++ packages/core/src/create-share/index.svelte.ts
@@ -0,0 +1,71 @@
+import { normalizeValue } from '../__internal__/utils.svelte.js';
+import { defaultNavigator, type ConfigurableNavigator } from '../__internal__/configurable.js';
+import type { MaybeGetter } from '../__internal__/types.js';
+
+type NavigatorShareData = {
+	title?: string;
+	files?: File[];
+	text?: string;
+	url?: string;
+};
+
+interface NavigatorWithShare {
+	share?: (data: NavigatorShareData) => Promise<void>;
+	canShare?: (data: NavigatorShareData) => boolean;
+}
+
+type CreateShareData = {
+	title?: MaybeGetter<string>;
+	files?: MaybeGetter<File[]>;
+	text?: MaybeGetter<string>;
+	url?: MaybeGetter<string>;
+};
+
+type CreateShareOptions = ConfigurableNavigator;
+
+type CreateShareReturn = {
+	readonly isSupported: boolean;
+	share(): Promise<void>;
+};
+
+/**
+ * Invokes the native sharing mechanism of the device to share data such as text, URLs, or files.
+ * @param data The data to share.
+ * @param options Additional options to customize the behavior.
+ */
+export function createShare(
+	data: CreateShareData,
+	options: CreateShareOptions = {}
+): CreateShareReturn {
+	const { navigator = defaultNavigator } = options;
+
+	const _data = $derived({
+		files: normalizeValue(data.files),
+		text: normalizeValue(data.text),
+		title: normalizeValue(data.title),
+		url: normalizeValue(data.url)
+	});
+
+	const _navigator = navigator as NavigatorWithShare;
+	const isSupported = $derived(_navigator && 'canShare' in _navigator);
+
+	async function share() {
+		if (!isSupported) return;
+		let granted = true;
+
+		if (data.files && _navigator.canShare) {
+			granted = _navigator.canShare({ files: normalizeValue(data.files) });
+		}
+
+		if (granted) {
+			return _navigator.share!(_data);
+		}
+	}
+
+	return {
+		get isSupported() {
+			return isSupported;
+		},
+		share
+	};
+}
---
+++ packages/website/src/lib/docs/core/create-share/Demo.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { createShare } from '$sv-use/core';
+	import { Button, Input } from '$lib/components/atoms/index.js';
+
+	let text = $state('A collection of Svelte 5 utilities.');
+	const data = $state({
+		title: () => 'SvelteUse',
+		text: () => text,
+		url: browser ? location.href : ''
+	});
+
+	const share = createShare(data);
+
+	async function startShare() {
+		try {
+			await share.share();
+		} catch (err) {
+			console.error(err);
+		}
+	}
+</script>
+
+<div class="relative flex w-full flex-col gap-2">
+	{#if share.isSupported}
+		<Input bind:value={text} placeholder="Note" />
+		<Button onclick={startShare}>Share</Button>
+	{:else}
+		<p class="dark:text-zinc-200">Web share is not supported in your browser :(</p>
+	{/if}
+</div>
