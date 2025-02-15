e11888481ad99885463ddc9dce89ce7a16e72def
Sajidur Rahman <rahmansajidur30@gmail.com>
2025-02-15 18:24:15 +0100

    docs: added 'esm-env' to build.rollupOptions.external

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
+	build: {
+		rollupOptions: {
+			external: ['esm-env']
+		}
+	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
